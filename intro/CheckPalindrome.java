b212a368eb90a7a66dd95357462b505da003dc2f
JamesNguyen <ptit.anh.nt@gmail.com>
2024-08-08 15:17:32 +0700

    Update CheckPalindrome.java

--- intro/CheckPalindrome.java
+++ intro/CheckPalindrome.java
@@ -1,4 +1,32 @@
 package intro;
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[memory limit] 1 GB
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+*/
 
 public class CheckPalindrome {
     boolean solution(String inputString) {
